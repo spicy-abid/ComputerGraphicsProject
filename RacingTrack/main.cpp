b636eb9bcd4140d96bf2a7a566c0ef6bf579a305
akashasmaul <akashasmaulhossain@gmail.com>
2022-11-30 17:37:51 +0600

    added rahat contrib

--- RacingTrack/main.cpp
+++ RacingTrack/main.cpp
@@ -3,6 +3,121 @@
 #include <GL/glut.h>
 #include <math.h>
 
+void drawLamp(){   //lamp up
+
+     ///L1
+     glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(-1.0f,-0.050f);
+      glVertex2f(-0.90f,-0.050f);
+      glVertex2f(-0.90f,0.050f);
+      glVertex2f(-1.0f,0.050f);
+      glEnd();
+       ///L2
+      glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(-0.450f,-0.050f);
+      glVertex2f(-0.350f,-0.050f);
+      glVertex2f(-0.350f,0.050f);
+      glVertex2f(-0.450f,0.050f);
+      glEnd();
+      ///L3
+      glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(0.10f,-0.050f);
+      glVertex2f(0.20f,-0.050f);
+      glVertex2f(0.20f,0.050f);
+      glVertex2f(0.10f,0.050f);
+      glEnd();
+      ///L4
+      glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(0.650f,-0.050f);
+      glVertex2f(0.750f,-0.050f);
+      glVertex2f(0.750f,0.050f);
+      glVertex2f(0.650f,0.050f);
+      glEnd();
+
+      //lamp stand
+      ///L1
+      glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(-0.970f,-0.40f);
+      glVertex2f(-0.930f,-0.40f);
+      glVertex2f(-0.930f,0.0f);
+      glVertex2f(-0.970f,0.0f);
+      glEnd();
+      ///L2
+      glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(-0.420f,-0.40f);
+      glVertex2f(-0.380f,-0.40f);
+      glVertex2f(-0.380f,0.0f);
+      glVertex2f(-0.420f,0.0f);
+      glEnd();
+      ///L3
+      glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(0.130f,-0.40f);
+      glVertex2f(0.170f,-0.40f);
+      glVertex2f(0.170f,0.0f);
+      glVertex2f(0.130f,0.0f);
+      glEnd();
+      ///L4
+      glBegin(GL_QUADS);
+      glColor3f(0.90f, 0.60f, 0.0f);
+      glVertex2f(0.680f,-0.40f);
+      glVertex2f(0.720f,-0.40f);
+      glVertex2f(0.720f,0.0f);
+      glVertex2f(0.680f,0.0f);
+      glEnd();
+
+      //lamp current line
+      ///L1
+      glLineWidth(2);
+      glBegin(GL_LINES);
+      glColor3f(0.0f, 0.0f, 0.0f);
+      glVertex2f(-0.950f,0.0f);
+      glVertex2f(-0.70f,-0.10f);
+      glVertex2f(-0.70f,-0.10f);
+      glVertex2f(-0.40f,0.0f);
+      glVertex2f(-0.40f,0.0f);
+      glVertex2f(-0.10f,-0.10f);
+      glVertex2f(-0.10f,-0.10f);
+      glVertex2f(0.150f,0.0f);
+      glVertex2f(0.150f,0.0f);
+      glVertex2f(0.450f,-0.10f);
+      glVertex2f(0.450f,-0.10f);
+      glVertex2f(0.70f,0.0f);
+      glVertex2f(0.70f,0.0f);
+      glVertex2f(1.0f,-0.10f);
+      glEnd();
+
+      //lamp current line2
+      ///L2
+      glLineWidth(2);
+      glBegin(GL_LINES);
+      glColor3f(0.0f, 0.0f, 0.0f);
+      glVertex2f(-0.950f,0.0f);
+      glVertex2f(-0.70f,-0.050f);
+      glVertex2f(-0.70f,-0.050f);
+      glVertex2f(-0.40f,0.0f);
+      glVertex2f(-0.40f,0.0f);
+      glVertex2f(-0.10f,-0.050f);
+      glVertex2f(-0.10f,-0.050f);
+      glVertex2f(0.150f,0.0f);
+      glVertex2f(0.150f,0.0f);
+      glVertex2f(0.450f,-0.050f);
+      glVertex2f(0.450f,-0.050f);
+      glVertex2f(0.70f,0.0f);
+      glVertex2f(0.70f,0.0f);
+      glVertex2f(1.0f,-0.050f);
+      glEnd();
+
+
+      glFlush();
+}
+
 void display()
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); // Set background color to black and opaque
@@ -200,7 +315,7 @@ void display()
 //	glVertex2f(-0.6f,-1.6f);
 //	glEnd();
 
-
+    drawLamp();
 
 	glFlush();
 }
